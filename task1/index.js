6882717beab09a25efbca72523a72da74d4b8ec3
Azat Muzapbar <azamuzapbar@gmail.com>
2023-04-04 18:29:28 +0600

    Выполнил первое задание

---
+++ task1/index.js
@@ -0,0 +1,8 @@
+const divs = document.getElementsByTagName('div');
+
+console.log(divs[2].textContent);
+console.log(divs[4].textContent);
+console.log(divs[1].textContent);
+console.log(divs[5].textContent);
+console.log(divs[3].textContent);
+console.log(divs[0].textContent);
